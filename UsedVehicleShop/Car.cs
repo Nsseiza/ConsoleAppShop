e72a68a4d462b7e081249b8c6f58734df311c4ca
Pedja <nsseiza@gmail.com>
2020-06-26 01:25:45 +0200

    Created models Vehicle,Car and Motorcycle. Added methods for displaying text to the console and writing to a file.

---
+++ UsedVehicleShop/Car.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace UsedVehicleShop
+{
+    class Car : Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumOfDoors { get; set; }
+        public List<string> Equipment { get; set; }
+
+        public Car(string brand, string model, int numOfDoors, List<string> equipment,
+            string advTitle, double price, string descrption, bool condition = true) : base(advTitle, price, descrption, condition)
+        {
+            Brand = brand;
+            Model = model;
+            NumOfDoors = numOfDoors;
+            Equipment = equipment;
+        }
+
+        public Car(string str) : base()
+        {
+            string[] tokens = str.Split(',');
+
+            AdvTitle = tokens[0];
+            Price = double.Parse(tokens[1]);
+            Descrption = tokens[2];
+            Brand = tokens[3];
+            Model = tokens[4];
+            NumOfDoors = int.Parse(tokens[5]);
+            Condition = bool.Parse(tokens[6]);
+            Deleted = bool.Parse(tokens[7]);
+            Equipment = new List<string>();
+
+            for (int i = 8; i < tokens.Length; i++)
+            {
+                Equipment.Add(tokens[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = base.ToString() + string.Format($"; brand: {Brand}; model: {Model}; doors: {NumOfDoors}; Equipment: ",
+                Brand, Model, NumOfDoors);
+
+            if (Equipment.Count == 0)
+            {
+                return str + "no equipment";
+            }
+
+            string strEquipment = "";
+            for (int i = 0; i < Equipment.Count; i++)
+            {
+                if (i == 0)
+                {
+                    strEquipment += string.Format($"{Equipment[i]}");
+                }
+                else
+                {
+                    strEquipment += string.Format($"{Equipment[i]}");
+                }
+            }
+
+            return str + strEquipment;
+        }
+
+        public override string ToFileString()
+        {
+            string firstPart = string.Format($"Car,{AdvTitle},{Price},{Descrption},{Brand},{Model},{NumOfDoors},{Condition},{Deleted}");
+
+            string secondPart = "";
+            for (int i = 0; i < Equipment.Count; i++)
+            {
+                secondPart += string.Format($",{Equipment[i]}");
+            }
+            return firstPart + secondPart;
+        }
+
+    }
+}
---
+++ UsedVehicleShop/Vehicle.cs
@@ -0,0 +1,55 @@
+﻿namespace UsedVehicleShop
+{
+    class Vehicle
+    {
+        private static int counterID = 0;
+
+        public int ID { get; set; }
+        public string AdvTitle { get; set; }
+        public double Price { get; set; }
+        public string Descrption { get; set; }
+        public bool Condition { get; set; }
+        public bool Deleted { get; set; }
+
+        public Vehicle()
+        {
+            ID = counterID;
+            counterID++;
+
+            AdvTitle = "";
+            Price = 0;
+            Descrption = "";
+            Condition = false;
+            Deleted = false;
+        }
+
+        public Vehicle(string advTitle, double price, string descrption, bool condition = true)
+        {
+            ID = counterID;
+            counterID++;
+
+            AdvTitle = advTitle;
+            Price = price;
+            Descrption = descrption;
+            Condition = condition;
+            Deleted = false;
+        }
+
+        public virtual string ToFileString()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"ID: {ID}; title: {AdvTitle} price: {Price}; condition: {(Condition ? "Available" : "Sold")}; description: {Descrption}");
+        }
+
+        public bool IsDeleted()
+        {
+            return Deleted;
+        }
+
+
+    }
+}
