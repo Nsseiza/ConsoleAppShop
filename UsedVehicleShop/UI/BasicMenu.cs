1d62b8a1fc6789895c54f42a567e3b07442e2669
Pedja <nsseiza@gmail.com>
2020-06-28 01:36:12 +0200

    Created option menu for entities

--- UsedVehicleShop/UI/BasicMenu.cs
+++ UsedVehicleShop/UI/BasicMenu.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using UsedVehicleShop.HelperClasses;
 
 namespace UsedVehicleShop.UI
 {
     class BasicMenu
     {
+        public static void Meni()
+        {
+            FirstMenu();
+        }
+
+        public static void FirstMenu()
+        {
+            while (true)
+            {
+                int opcija = MainMenus.ExecuteMenu(MenuPrint, 6);
+
+                if (opcija == 1)
+                    EntryDeleteEditUI.Menu();
+
+                else if (opcija == 2)
+                    OverviewUI.Menu();
+
+                else if (opcija == 3)
+                    Console.WriteLine("In Progress");
+
+
+                else if (opcija == 4)
+                    Console.WriteLine("In Progress");
+
+                else if (opcija == 5)
+                    Console.WriteLine("In Progress");
+
+                else if (opcija == 6)
+                    break;
+            }
+        }
+
         public static void MenuPrint()
         {
             Console.WriteLine("1 - Enter, delete and modify entities");
@@ -11,6 +44,7 @@ namespace UsedVehicleShop.UI
             Console.WriteLine("3 - Entity Search");
             Console.WriteLine("4 - Invoicing");
             Console.WriteLine("5 - Bills overview");
+            Console.WriteLine("5 - Exit");
         }
     }
 }
