933b20ced14d62761b78126c14264896bf645942
Pedja <nsseiza@gmail.com>
2020-06-27 23:48:24 +0200

    Created class to overview and add, delete, edit entity with options menu.

---
+++ UsedVehicleShop/UI/EntryDeleteEditUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UsedVehicleShop.UI
+{
+    class EntryDeleteEditUI
+    {
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Possible operations:");
+            Console.WriteLine("1 - Enter new entity");
+            Console.WriteLine("2 - Delete existing entity");
+            Console.WriteLine("3 - Modify Entities");
+            Console.WriteLine("4 - Back to previous menu");
+        }
+
+        public static void Menu()
+        {
+            //...
+        }
+    }
+}
---
+++ UsedVehicleShop/UI/OverviewUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsedVehicleShop.UI
+{
+    class OverviewUI
+    {
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Possible operations:");
+            Console.WriteLine("1 - Overview of all entities");
+            Console.WriteLine("2 - Overview of existing entities");
+            Console.WriteLine("3 - Overview of deleted entities");
+            Console.WriteLine("4 - Overview of sold entities (vehicles only)");
+            Console.WriteLine("5 - Overview of unsold entities (vehicles only)");
+            Console.WriteLine("6 - Back to previous menu");
+        }
+
+        public static void Menu()
+        {
+            //...
+        }
+    }
+}
